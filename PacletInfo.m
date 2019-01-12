cd7658fb6f8688e678c5778c809d8ebe117adee9
groenkek <groenkek@hu-berlin.de>
2019-01-13 00:50:28 +0100

    Third time's a charm

---
+++ PacletInfo.m
@@ -0,0 +1,16 @@
+(* Paclet Info File *)
+
+(* created 2019/01/13*)
+
+Paclet[
+    Name -> "HUPP",
+    Version -> "0.0.1",
+    MathematicaVersion -> "11+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}, 
+            {"Kernel", Root -> ".", Context -> "HUPP`"}
+        }
+]
+
+
